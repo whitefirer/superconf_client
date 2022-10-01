85c6b5168345fa46f9c0e424000cdd733d97c6db
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-01 14:05:12 +0000

    Adding tarfile member sanitization to extractall()

--- superconf_client.py
+++ superconf_client.py
@@ -424,7 +424,26 @@ class SuperconfClient:
         extract_path = f'/tmp/{backup_name}'
 
         with tarfile.open(backup_file) as tar:
-            tar.extractall(extract_path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner) 
+                
+            
+            safe_extract(tar, extract_path)
             for tarinfo in tar:
                 if tarinfo.isdir():
                     print(tarinfo.name, "is", tarinfo.size,
